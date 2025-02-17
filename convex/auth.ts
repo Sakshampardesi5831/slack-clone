d92f0650e3f2e886c21f75bd69d2d0acb5ae0488
Saksham Pardesi <sakshampardesi5831@gmail.com>
2025-02-18 00:15:24 +0530

    User Imformation with avatar complete

--- convex/auth.ts
+++ convex/auth.ts
@@ -3,6 +3,17 @@ import Github from "@auth/core/providers/github";
 import Google from "@auth/core/providers/google";
 import { Password } from "@convex-dev/auth/providers/Password";
 import { Email } from "@convex-dev/auth/providers/Email";
+import { DataModel } from "./_generated/dataModel";
+
+export default Password<DataModel>({
+  profile(params) {
+    return {
+      email: params.email as string,
+      name: params.name as string,
+    };
+  },
+});
+
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [Github, Google,Password],
 });
